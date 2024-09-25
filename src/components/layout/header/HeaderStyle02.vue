1c9586169d0ba9cf82e11e4cd8ea33b2be3b1507
kuei <momonbin1991@icloud.com>
2024-09-25 13:33:55 +0800

    '20240925_header功能更新'

--- src/components/layout/header/HeaderStyle02.vue
+++ src/components/layout/header/HeaderStyle02.vue
@@ -1,5 +1,10 @@
 <template>
-  <header id="header" class="headerStyle02 open-wrap-box">
+  <header
+    id="header"
+    class="headerStyle02 open-wrap-box"
+    ref="header"
+    :class="{ scroll: isScrolled, 'finish-banner': finishBanner }"
+  >
     <div class="header-navbar-box">
       <a href="/" class="d-flex">
         <img src="/images/header/centurion_logo.png" alt="logo" class="main-logo" />
@@ -22,6 +27,62 @@ export default {
   components: {
     NavbarHeader,
     FullMenu
+  },
+  data() {
+    return {
+      isScrolled: false,
+      finishBanner: false,
+      hdTop: 0,
+      bannerTop: 0
+    }
+  },
+  // mounted 掛載完成
+  mounted() {
+    this.hdTop = this.$refs.header.offsetHeight
+
+    this.bannerTop = document.querySelector('.pic-box')?.offsetHeight || 0
+
+    window.addEventListener('scroll', this.handleScroll)
+
+    if (document.body.classList.contains('index')) {
+      // 初始化首頁滾動效果
+      this.handleScroll()
+    }
+  },
+
+  beforeUnmount() {
+    window.removeEventListener('scroll', this.handleScroll)
+  },
+  methods: {
+    handleScroll() {
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop
+
+      if (document.body.classList.contains('index')) {
+        if (scrollTop > this.hdTop) {
+          this.isScrolled = true
+          this.finishBanner = false
+        } else {
+          this.isScrolled = false
+          if (document.querySelector('.bannerBlock')?.classList.contains('moveUp-banner')) {
+            this.finishBanner = true
+            document.boby.style.overflow = ''
+            document.body.style.touchAction = 'none'
+            document.body.style.webkitUserSelect = 'none'
+            document.body.style.webkitTouchCallout = 'none'
+            document.body.style.webkitUserDrag = 'none'
+            document.body.style.webkitTapHighlightColor = 'rgba(0, 0, 0, 0)'
+          }
+        }
+      } else {
+        // 非首頁滾動邏輯
+        console.log('isScrolled', this.isScrolled)
+        if (scrollTop > this.hdTop) {
+          this.isScrolled = true
+        } else {
+          this.isScrolled = false
+        }
+      }
+    }
   }
 }
 </script>
