aaa36a04a9d165d0703b535262e1b4c02f587c52
kuei <momonbin1991@icloud.com>
2024-09-25 11:00:53 +0800

    'footer初版完成'

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,18 @@
     <headerStyle02></headerStyle02>
     <div class="header-fullMenu-mask"></div>
     <router-view></router-view>
+    <footer-style-01></footer-style-01>
   </div>
 </template>
 
 <script>
 import HeaderStyle02 from './components/layout/header/HeaderStyle02.vue'
+import FooterStyle01 from './components/layout/footer/FooterStyle01.vue'
 
 export default {
   components: {
-    HeaderStyle02
+    HeaderStyle02,
+    FooterStyle01
   }
 }
 </script>
